d4dae562887cc4a31ae0d46f096fbbd41aea7d9a
Hziska <116638849+Hziska@users.noreply.github.com>
2022-11-10 05:44:43 +0700

    Css

---
+++ Style.js
@@ -0,0 +1,41 @@
+body{
+    background-color: white;
+}
+h1{
+    text-align: center;
+}
+.hasil,.jmlUang{
+    margin: auto;
+    width: 150px;
+    height: 70px;
+    box-shadow: 0 0 10px black;
+    border-radius: 5px;
+    background-color: white;
+    float:left;
+    margin-left: 10px;
+}
+.container{
+    width: 340px;
+    height: 200px;
+    margin: auto;
+}
+.angkot{
+    width: 300px;
+    height: 300px;
+}
+.button{
+    position: relative;
+}
+.skor{
+    text-align: center;
+    font-size: 25px;
+}
+.rules{
+    position: relative;
+    top:-340;
+    left:50;
+}
+.bg{
+    position:absolute ;
+    top:230;
+}
